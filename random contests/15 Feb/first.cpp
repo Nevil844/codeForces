85332812b68a6f2f7053cf0e64d78d73577f7c7c
Nevil844 <nevil.j@ahduni.edu.in>
2024-02-16 13:38:52 +0530

    15feb-3

--- random contests/15 Feb/first.cpp
+++ random contests/15 Feb/first.cpp
@@ -5,15 +5,19 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        long long k,x,a;
+        long long k,x,a, sum=0;
         cin>>k>>x>>a;
-        int kt=k*(k+1);
-        // if(a<=x) cout<<"NO\n";
-        // else{
-        //     if(k<=x) cout<<"NO\n";
-        //     else cout<<"YES\n";
-        // }
-        if(a<kt) cout<<"NO"<<endl;
+        k--;
+        bool ans=1;
+        for(int i=0; i<=x; i++){
+            int bet=(sum+k)/k;
+            sum+=bet;
+            if(a<sum){
+                ans=0;
+                break;
+            }
+        }
+        if(!ans) cout<<"NO"<<endl;
         else cout<<"YES"<<endl;
     }
 }
