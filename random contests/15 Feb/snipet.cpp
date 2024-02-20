486a9bed65fb2673788648ff99efce6e7dd53784
Nevil844 <nevil.j@ahduni.edu.in>
2024-02-20 18:40:19 +0530

    19feb

--- random contests/15 Feb/snipet.cpp
+++ random contests/15 Feb/snipet.cpp
@@ -6,6 +6,13 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        
+        string s;
+        cin>>s;
+        int count=0;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='A') count++;
+        }
+        if(count>2) cout<<'A'<<endl;
+        else cout<<'B'<<endl;
     }
 }
