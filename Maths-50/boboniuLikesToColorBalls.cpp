b8176d0842e2c1787bf68a44cd56946f218acfff
Nevil844 <nevil.j@ahduni.edu.in>
2024-02-26 00:05:11 +0530

    37

--- Maths-50/boboniuLikesToColorBalls.cpp
+++ Maths-50/boboniuLikesToColorBalls.cpp
@@ -1,32 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool check(long long r,long long g,long long b,long long w){
+    if(r%2==0  && g%2==0 && b%2==0) return 1;
+    else if(r%2==0 && b%2==0 && w%2==0) return 1;
+    else if(r%2==0 && g%2==0 && w%2==0) return 1;
+    else if(b%2==0 && g%2==0 && w%2==0) return 1;
+    return 0;
+}
+
 int main(){
     int t;
     cin>>t;
     while(t--){
         long long r,g,b,w;
         cin>>r>>g>>b>>w;
-        bool count=0, ans=0;
-        while(r>=0 && g>=0 && b>=0){
-            if(r%2==0  && g%2==0 && b%2==0) count=1;
-            else if(r%2==0 && b%2==0 && w%2==0) count=1;
-            else if(r%2==0 && g%2==0 && w%2==0) count=1;
-            else if(b%2==0 && g%2==0 && w%2==0) count=1;
-
-            if(count){
-                cout<<"Yes\n";
-                ans=1;
-                break;
-            }else{
-                r--;
-                g--;
-                b--;
-                w+=3;
-            }
-        }
-        if(!ans){
-            cout<<"No\n";
-        }
+        if(check(r,g,b,w)) cout<<"Yes\n";
+        else if(r>0 && g>0 && b>0 && check(r-1,g-1,b-1,w+1))  cout<<"Yes\n";
+        else cout<<"No\n";
     }
 }
