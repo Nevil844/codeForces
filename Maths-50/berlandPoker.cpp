25faf9187366cfb6542372644b1f83a63f6b6347
Nevil844 <nevil.j@ahduni.edu.in>
2024-03-05 01:06:54 +0530

    41

--- Maths-50/berlandPoker.cpp
+++ Maths-50/berlandPoker.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int main(){
-    int n,m,k;
-    cin>>n>>m>>k;
-    
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,k;
+        cin>>n>>m>>k;
+        int cards=n/k;
+        if(cards>=m) cout<<m;
+        else {
+            m-=cards;
+            int x=ceil(double(m)/(k-1));
+            cout<<cards-x;
+        }
+        cout<<endl;
+    }
 }
