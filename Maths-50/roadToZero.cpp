832e493fd7fb970dd0dd3a497dbaabb482376464
Nevil844 <nevil.j@ahduni.edu.in>
2024-03-08 00:16:41 +0530

    42

--- Maths-50/roadToZero.cpp
+++ Maths-50/roadToZero.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main(){
     int t;
     cin>>t;
-    long long x,y,a,b;
-    cin>>x>>y>>a>>b;
+    while(t--){
+        long long x,y,a,b;
+        cin>>x>>y>>a>>b;
+        b=min(b,2*a);
+        cout<<(min(x,y)*b)+(abs(x-y)*a)<<endl;
+    }
 }
