fad5d07b1978d83447bcfff9c93601cd3c9ef1af
Nevil844 <nevil.j@ahduni.edu.in>
2024-03-12 11:13:35 +0530

    45

--- Maths-50/GCDLength.cpp
+++ Maths-50/GCDLength.cpp
@@ -5,6 +5,8 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        
+        int a,b,c;
+        cin>>a>>b>>c;
+        cout<<"1"<<string((a-1),'0')<<" "<<string((b-c+1),'1')<<string(c-1,'0')<<endl;
     }
 }
